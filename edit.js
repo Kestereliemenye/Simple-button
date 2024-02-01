1ce9c14db0576a375d56a66a2fcd554d1a32df28
kester eliemenye <kestereliemenye@gmail.com>
2024-02-01 04:05:24 +0100

    edit button done well

--- edit.js
+++ edit.js
@@ -2,8 +2,17 @@ const content = document.querySelector('.word');
 const btn = document.querySelector('.edit-btn');
 const input = document.createElement('input');
 
+
 btn.addEventListener('click', edit);
 function edit(e){
-    content.replaceWith(input);
-
+    let text = content.innerHTML;
+    input.type = 'text';
+    input.value= text;
+    content.innerHTML = '' ;
+    content.appendChild(input);
+    input.focus();
+    input.setSelectionRange(text.length, text.length);
+    
 }
+
+console.log(content.value);
